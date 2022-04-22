b41a06735f72f8355d15a2b95f5283850649459f
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-04-22 11:14:05 -0400

    latest version

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
     has_many :pokemon, through: :user_favorites
 
     has_secure_password
-    validates :username, uniquness: true, presence: true
+    validates :username, uniqueness: true, presence: true
 end
