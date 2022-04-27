f27d4a042e0ccc3f0ad3ef428a6766350e15b1c5
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-04-27 14:35:59 -0400

    root added to route'

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   # Defines the root path route ("/")
   # root "articles#index"
 end
